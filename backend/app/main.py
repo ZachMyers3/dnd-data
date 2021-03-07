a38f1c200d4f8e65e53b6a9d63c7e519c3fc9c4d
Zach Myers <zachmyers3@gmail.com>
2021-03-07 13:21:05 -0500

    fix typo

--- backend/app/main.py
+++ backend/app/main.py
@@ -33,7 +33,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    expost_headers=["*"],
+    expose_headers=["*"],
 )
 
 if __name__ == "__main__":
