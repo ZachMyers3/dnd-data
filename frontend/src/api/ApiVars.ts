e20c751238c0bf601916bbf33c2068b429f373cb
Zach Myers <zachmyers3@gmail.com>
2021-03-14 13:41:04 -0400

    dynamic api url depending on environment

--- frontend/src/api/ApiVars.ts
+++ frontend/src/api/ApiVars.ts
@@ -1,3 +1,7 @@
-// export const API_URL = "http://localhost:8000/api/v1";
-export const API_URL =
-  window.location.protocol + "//" + window.location.host + "/api/v1";
+export let API_URL = "";
+
+if (process.env.NODE_ENV === "development") {
+  API_URL = "http://localhost:8000/v1";
+} else {
+  API_URL = window.location.protocol + "//" + window.location.host + "/api/v1";
+}
